ef331635f29a81af7bf84b89e472cc82ad97d109
3xp1o1t <jes.monti.503@gmail.com>
2023-09-05 15:23:34 -0600

    Added: Grid system for homepage
    Added Topbox component for first grid box.
    Added data for Topbox (top deals).
    Added grid structure for Home page.
    Modified title case for Menu in menu.scss.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,22 @@
+import Topbox from '../../components/topBox/Topbox';
 import './home.scss';
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      {/* .box.box$*9{box$} */}
+      <div className="box box1">
+        <Topbox />
+      </div>
+      <div className="box box2">box2</div>
+      <div className="box box3">box3</div>
+      <div className="box box4">box4</div>
+      <div className="box box5">box5</div>
+      <div className="box box6">box6</div>
+      <div className="box box7">box7</div>
+      <div className="box box8">box8</div>
+      <div className="box box9">box9</div>
+    </div>
+  );
 };
 
 export default Home;
