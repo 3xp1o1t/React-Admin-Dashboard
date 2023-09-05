037b9f37b0ab8f4725cdc0bb9e0ba7f5d8a9233a
3xp1o1t <jes.monti.503@gmail.com>
2023-09-04 20:45:39 -0600

    Added: Footer styles

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import './footer.scss';
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <span>3xp1o1t</span>
+      <span>&copy; 2023 Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
