5da3b3ea4948e0d6db8184787bb3e51ca9342501
3xp1o1t <jes.monti.503@gmail.com>
2023-09-04 20:02:25 -0600

    Added: Layout basic setup
    Added navbar, menu, footer and some components
    Added styles for each component
    Added global.scss styles
    Modified router structure to render children components using Outlet.
    Added login router outside our layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,59 @@
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Menu from './components/menu/Menu';
+import Navbar from './components/navbar/Navbar';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Products from './pages/products/Products';
+import Users from './pages/users/Users';
+import './styles/global.scss';
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  // Layout
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            {/* render dinamically children components */}
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  // navigation
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/users',
+          element: <Users />,
+        },
+        {
+          path: '/products',
+          element: <Products />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
